49184f4b5c2b983da72f33a623154efaad4087f4
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-30 13:31:07 +0200

    scaffolding completed

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+    /*
+        !SNACK 1
+        *Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
+        *Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+        *Olimpia Milano - Cantù | 55-60
+
+        !SNACK 2
+        *Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+        *che mail contenga un punto e una chiocciola e che age sia un numero. 
+        *Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+        !SNACK 3
+        *Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+        *Stampare ogni data con i relativi post.
+        *Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
+        !SNACK 4
+        *Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+        !SNACK 5
+        *Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
+        !SNACK 6
+        *Utilizzare questo array: https://pastebin.com/CkX3680A. Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde.
+
+        !SNACK 7
+        *Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
+        *Stampare Nome, Cognome e la media dei voti di ogni alunno.
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- css -->
+    <script src="css/style.css"></script>
+    <title>PHP Snack</title>
+</head>
+<body>
+    <div class="container">
+    </div>
+</body>
+</html>
